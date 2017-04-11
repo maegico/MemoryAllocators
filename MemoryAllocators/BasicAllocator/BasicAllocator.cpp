9a56231e0989f34265ba3fd0499f9e672f1e0be0
Sean Connolly <seanconnolly@mail.rit.edu>
2017-04-10 20:10:59 -0400

    Fixed problem and learned a new one.
    This currently only works for int32_t variables. I have to figure out a
    way to make this generic. It seems that I have to make a generic solution.
    Also, I need to address the lack of memory alignment handling.

--- MemoryAllocators/BasicAllocator/BasicAllocator.cpp
+++ MemoryAllocators/BasicAllocator/BasicAllocator.cpp
@@ -21,14 +21,16 @@ void BasicAllocator::Destroy()
 
 void* BasicAllocator::Alloc(void* data, uint32_t size)
 {
-	for (uint64_t i = 0; i < MAX_BUFFER_SIZE; i++)
+	for (uint64_t i = 0; i < MAX_BUFFER_SIZE;)
 	{
 		int32_t* ptr = (int32_t*)MemoryBuffer + i;
 		if (*ptr == NULL || *ptr > 0)
 		{
-			int32_t header = -size;
+			int32_t header = -size/1;
 			*ptr = header;
-			*(ptr + 1) = *(int32_t*)data;
+			*(ptr + 1) = *(int32_t*)data;	//this only works with an int32_t
+												//casts a float to an int, causing truncation
+												//saving it into an int will also cause the same problem
 
 			//*((byte*)MemoryBuffer + i) = *(byte*)data;
 			usedMem += size + 4;
@@ -38,7 +40,7 @@ void* BasicAllocator::Alloc(void* data, uint32_t size)
 		else if(*ptr < 0)
 		{
 			//uint32_t* place = (uint32_t*)MemoryBuffer + i;
-			i -= *ptr;
+			i -= (*ptr/4)-1;
 		}
 	}
 	return nullptr;	//place holder
@@ -62,9 +64,13 @@ uint32_t BasicAllocator::getUsedMem()
 
 void BasicAllocator::showMem(void* position)
 {
-	for (int i = 0; i < 32; i++)
+	for (int i = 0; i < 4; i++)
 	{
-		printf("MemoryBuffer ptrs: 0x%x\n", ((int32_t*)MemoryBuffer + i));
-		//printf("Here is the mem: %d\n", *((int32_t*)MemoryBuffer + i));
+		//printf("MemoryBuffer ptrs: 0x%x\n", ((int32_t*)MemoryBuffer + i));
+		printf("Here is the mem: %d\n", *((int32_t*)MemoryBuffer + i));
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		printf("Here is the mem: %d\n", *((float*)MemoryBuffer + i));
 	}
 }
--- MemoryAllocators/BasicAllocator/Source.cpp
+++ MemoryAllocators/BasicAllocator/Source.cpp
@@ -27,28 +27,35 @@ int main()
 	DEBUG(Debug::Log("Used Memory: " + std::to_string(BasicAllocator::getUsedMem())));
 	int data = 52;
 	int secondData = 78;
-	int* dataPtr;
-	TIMER_MICRO(dataPtr = (int*)BasicAllocator::Alloc(&data, sizeof(data)));
-	printf("Data Ptr is: %d\n", dataPtr);
-	printf("Data is %d\n", *dataPtr);
-
-	int* secondDataPtr;
-	TIMER_MICRO(secondDataPtr = (int*)BasicAllocator::Alloc(&secondData, sizeof(secondData)));
-	printf("2nd Data Ptr is: %d\n", secondDataPtr);
-	printf("2nd Data is %d\n", *secondDataPtr);
-	printf("Data Ptr is: %d\n", dataPtr);
-	printf("Data is %d\n", *dataPtr);
-
-	//BasicAllocator::Dealloc(dataPtr);
+	float data3 = 0.5f;
 
 	//int* dataPtr;
-	TIMER_MICRO(dataPtr = (int*)BasicAllocator::Alloc(&secondData, sizeof(secondData)));
-	printf("2nd Data Ptr is: %d\n", secondDataPtr);
-	printf("2nd Data is %d\n", *secondDataPtr);
-	printf("Data Ptr is: %d\n", dataPtr);
-	printf("Data is %d\n", *dataPtr);
+	//TIMER_MICRO(dataPtr = (int*)BasicAllocator::Alloc(&data, sizeof(data)));
+	///*printf("Data Ptr is: %d\n", dataPtr);
+	//printf("Data is %d\n", *dataPtr);*/
 
-	BasicAllocator::showMem(secondDataPtr);
+	//int* secondDataPtr;
+	//TIMER_MICRO(secondDataPtr = (int*)BasicAllocator::Alloc(&secondData, sizeof(secondData)));
+	///*printf("2nd Data Ptr is: %d\n", secondDataPtr);
+	//printf("2nd Data is %d\n", *secondDataPtr);
+	//printf("Data Ptr is: %d\n", dataPtr);
+	//printf("Data is %d\n", *dataPtr);*/
+
+	////BasicAllocator::Dealloc(dataPtr);
+	//secondData = 9;
+	////int* dataPtr;
+	//TIMER_MICRO(dataPtr = (int*)BasicAllocator::Alloc(&secondData, sizeof(secondData)));
+	///*printf("2nd Data Ptr is: %d\n", secondDataPtr);
+	//printf("2nd Data is %d\n", *secondDataPtr);
+	//printf("Data Ptr is: %d\n", dataPtr);
+	//printf("Data is %d\n", *dataPtr);*/
+
+	float* data3Ptr;
+	TIMER_MICRO(data3Ptr = (float*)BasicAllocator::Alloc(&data3, sizeof(data3)));
+	printf("Float Ptr is: %d\n", data3Ptr);
+	printf("Float is %f\n", *data3Ptr);
+
+	//BasicAllocator::showMem(secondDataPtr);
 
 	DEBUG(Debug::Log("Free Memory: " + std::to_string(BasicAllocator::getFreeMem())));
 	DEBUG(Debug::Log("Used Memory: " + std::to_string(BasicAllocator::getUsedMem())));
